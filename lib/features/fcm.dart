7d61ebb974f61eed78e76a9ce0953d2c26cbefd3
Suhaila Ramadan <suhailaramadan23@gmail.com>
2025-11-18 18:42:12 +0200

    fcm.dart

--- lib/features/fcm.dart
+++ lib/features/fcm.dart
@@ -2,8 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-  // If you're going to use other Firebase services in the background, such as Firestore,
-  // make sure you call `initializeApp` before using other Firebase services.
+  
   await Firebase.initializeApp();
 
   print("Handling a background message: ${message.messageId}");
@@ -35,3 +34,4 @@ class Fcm {
     getToken();
   }
 }
+
